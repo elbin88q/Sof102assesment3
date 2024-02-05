f51b41ff4ff0e9c263c894b99899f60107c865b5
elbin88q <109290568+elbin88q@users.noreply.github.com>
2024-02-05 15:47:24 +1100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="site-footer">
+        <div class="footer-container">
+            <!-- Footer sections here -->
+            <div class="footer-section">
+                <h5>Help & Support</h5>
+                <ul>
+                    <li><a href="#">Frequently Asked Questions</a></li>
+                    <li><a href="#">Track my order</a></li>
+                    <li><a href="#">Vendor Support</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="footer-social">
+                <h5>Get Social</h5>
+                <ul>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">YouTube</a></li>
+                    <li><a href="#">LinkedIn</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
---
+++ nav.php
@@ -0,0 +1,14 @@
+<header>
+        <h1>E-commerce store</h1>
+    </header>
+
+<nav>
+            <ul id="nav-list">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="categories.html">Products</a></li>
+                <li><a href="aboutus.html">About</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="search.html">Search</a></li>
+            </ul>
+        </nav>
+   
